f2ec655f60cc23724c0450d857b6bfcc1ad240db
thienhuyz <thienhuyz3006@gmail.com>
2026-08-27 23:08:25 +0700

    feat: refactor server initialization and improve error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,24 @@ import 'dotenv/config'
 import express from 'express'
 import databaseService from '~/services/database.services'
 import usersRouter from '~/routes/users.routes'
-import { defaultErrorHandler } from './middlewares/error.middlewares'
+import { defaultErrorHandler } from '~/middlewares/error.middlewares'
 
 const app = express()
-const PORT = 3000
+const PORT = Number(process.env.PORT) || 3000
 
 app.use(express.json())
 app.use('/users', usersRouter)
-databaseService.connect()
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' })
+})
 app.use(defaultErrorHandler)
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`)
-})
+const bootstrap = async () => {
+  await databaseService.connect()
+
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+  })
+}
+
+bootstrap()
