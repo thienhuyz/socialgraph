0f0a96b2d5c926e268f2f05227fc8b59dec5ab91
thienhuyz <thienhuyz3006@gmail.com>
2026-09-15 16:11:46 +0700

    feat: add filter middleware for user update request body

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -12,6 +12,7 @@ import {
   getMeController,
   updateMeController
 } from '~/controllers/users.controllers'
+import { filterMiddleware } from '~/middlewares/common.middlewares'
 import {
   loginValidator,
   registerValidator,
@@ -24,6 +25,7 @@ import {
   verifyValidator,
   updateMeValidator
 } from '~/middlewares/users.middlewares'
+import { UpdateMeReqBody } from '~/models/requests/User.requests'
 import { wrapRequestHandler } from '~/utils/handlers'
 
 const usersRouter = Router()
@@ -47,6 +49,16 @@ usersRouter.patch(
   accessTokenValidator,
   verifyValidator,
   updateMeValidator,
+  filterMiddleware<UpdateMeReqBody>([
+    'name',
+    'date_of_birth',
+    'bio',
+    'location',
+    'website',
+    'username',
+    'avatar',
+    'cover_photo'
+  ]),
   wrapRequestHandler(updateMeController)
 )
 
